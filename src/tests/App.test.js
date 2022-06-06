7586caa939dbea675a0a362b8a7525cd853a1ac5
StuartCowley <voidstalker@live.co.uk>
2022-06-06 19:59:35 +0100

    Alert component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "../components/App";
 
 test("renders surreal estate logo with correct alt text", () => {
-  render(<App />);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
   const logoAltText = screen.getByAltText(/Site logo/);
 
   expect(logoAltText).toBeInTheDocument();
--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 test("renders all menu options", () => {
-  render(<NavBar />);
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
   const viewPropertiesLink = screen.getByText(/View Properties/);
   const addPropertyLink = screen.getByText(/Add a Property/);
 
