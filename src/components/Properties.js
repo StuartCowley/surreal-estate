1ad24823843fc1f8df984325c96f8043e4003238
StuartCowley <voidstalker@live.co.uk>
2022-06-08 23:29:49 +0100

    properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import PropertyCard from "./PropertyCard";
+import Alert from "./Alert";
+
+import "../styles/properties.css";
 
 const Properties = () => {
-  return <div>Properties Page</div>;
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState({ message: "" });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/PropertyListing/")
+      .then((res) => {
+        setProperties(res.data);
+        setAlert({
+          message: "",
+        });
+      })
+      .catch(() => {
+        setAlert({
+          message: "Server error. Please try again later.",
+        });
+      });
+  }, []);
+
+  return (
+    <>
+      <Alert message={alert.message} />
+      <div className="properties">
+        {properties.map((property) => (
+          <div key={property._id} className="item">
+            <PropertyCard {...property} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Properties;
