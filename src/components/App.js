73c30eb218d22abf37cf79fe8b76dd5b1933a6ef
StuartCowley <voidstalker@live.co.uk>
2022-09-18 13:53:57 +0100

    routing step

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 
 import "../styles/app.css";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 const App = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Properties />} />
+        <Route path="add-property" element={<AddProperty />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
