33a9f07bec5628352a4ebdf565976c913fc6b442
StuartCowley <voidstalker@live.co.uk>
2022-09-18 18:21:45 +0100

    Sort by price

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import qs from "qs";
 
 import "../styles/sidebar.css";
+
+const buildQueryString = (operation, valueObj) => {
+  const { search } = useLocation();
+
+  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+
+  const newQueryParams = {
+    ...currentQueryParams,
+    [operation]: JSON.stringify(valueObj),
+  };
+
+  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+};
 
 const SideBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -25,19 +39,56 @@ const SideBar = () => {
         <ul className="sidebar__list">
           <li className="sidebar__list-item">
             <NavLink
-              to={`?query={"city": "Manchester"}`}
+              to={buildQueryString("query", { city: "Manchester" })}
               className="sidebar-link"
             >
               Manchester
             </NavLink>
           </li>
-
           <li className="sidebar__list-item">
             <NavLink
-              to={`?query={"city": "Liverpool"}`}
+              to={buildQueryString("query", { city: "Leeds" })}
+              className="sidebar-link"
+            >
+              Leeds
+            </NavLink>
+          </li>
+          <li className="sidebar__list-item">
+            <NavLink
+              to={buildQueryString("query", { city: "Sheffield" })}
+              className="sidebar-link"
+            >
+              Sheffield
+            </NavLink>
+          </li>
+          <li className="sidebar__list-item">
+            <NavLink
+              to={buildQueryString("query", { city: "Liverpool" })}
               className="sidebar-link"
             >
               Liverpool
+            </NavLink>
+          </li>
+        </ul>
+      </section>
+      <section className="sidebar__sort">
+        <h3 className="sidebar__header">Sort by</h3>
+        <ul className="sidebar__list">
+          <li className="sidebar__list-item">
+            <NavLink
+              to={buildQueryString("sort", { price: 1 })}
+              className="sidebar-link"
+            >
+              Price Ascending
+            </NavLink>
+          </li>
+
+          <li className="sidebar__list-item">
+            <NavLink
+              to={buildQueryString("sort", { price: -1 })}
+              className="sidebar-link"
+            >
+              Price Descending
             </NavLink>
           </li>
         </ul>
